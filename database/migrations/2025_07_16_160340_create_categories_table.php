18d3a2a3647c1e6a22df19f14b14613ddb257725
marcoscondejr <marcos14conde@hotmail.com>
2025-07-17 00:01:24 -0300

    feat: create seeder to categories and primary data

--- database/migrations/2025_07_16_160340_create_categories_table.php
+++ database/migrations/2025_07_16_160340_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('code', 10);
-            $table->string('title', 100);
+            $table->string('name', 100);
             $table->string('description', 200)->nullable();
             $table->timestamps();
         });
