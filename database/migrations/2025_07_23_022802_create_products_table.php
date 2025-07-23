045c246594e333a0e2472d8f40fd18f6e57be110
marcoscondejr <marcos14conde@hotmail.com>
2025-07-22 23:54:29 -0300

    feat: creating migration of products

---
+++ database/migrations/2025_07_23_022802_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->string('description', 200)->nullable();
+            $table->foreignId('category_id')
+                  ->constrained()
+                  ->on('categories')
+                  ->onDelete('restrict');
+            $table->decimal('cost_price', 18,2);
+            $table->decimal('sale_price', 18, 2);
+            $table->integer('quantity_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
