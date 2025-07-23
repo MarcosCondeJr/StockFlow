5ddd9885b4a10903aece685cff1f188e3278c2ab
marcoscondejr <marcos14conde@hotmail.com>
2025-07-23 00:25:10 -0300

    wip

---
+++ app/Http/Requests/Product/ProductUpdateRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // gets the id of the parameter to ignore in validation
+        $productId = $this->route('product');
+
+        return [
+            'code'           => 'required|string|unique:products,code,'. $productId . '|max:10|min:3',
+            'name'           => 'required|string|max:150',
+            'description'    => 'nullable|string|max:200',
+            'category_id'    => 'required|integer',
+            'cost_price'     => 'required|numeric',
+            'sale_price'     => 'required|numeric',
+            'quantity_stock' => 'required|integer'
+        ];
+    }
+}
